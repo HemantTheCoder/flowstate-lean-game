443b4fd66c4b6d92265bda50f91b493708f7c058
Hemant <hemantreso9@gmail.com>
2026-02-01 15:20:25 +0530

    chore: added vite config for deployment

---
+++ client/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [react()],
+    base: '/flowstate-lean-game/', // Deployment Base URL for GitHub Pages
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    build: {
+        outDir: 'dist',
+    }
+});
